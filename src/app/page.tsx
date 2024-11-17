ad10b095b0277c731d6f4625907ca4e67de9b531
Adam <147444558+usradam@users.noreply.github.com>
2024-11-17 00:02:37 -1000

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,19 @@ import { Col, Container, Row } from 'react-bootstrap';
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
+    <Container id="landing-page-translation" fluid className="py-5">
       <Row className="align-middle">
         <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>On-demand menu translations</h1>
-          <p>Translate any menu item and select any currency you want</p>
+          <h2>On-demand menu translations</h2>
+          <p>Translate any menu item you want</p>
+        </Col>
+      </Row>
+    </Container>
+    <Container id="landing-page-currency" fluid className="py-5">
+      <Row className="align-middle">
+        <Col xs={8} className="d-flex flex-column justify-content-center ms-auto">
+          <h2>Choose any currency</h2>
+          <p>Pick a currency to display alongside the dollar value of an item</p>
         </Col>
       </Row>
     </Container>
