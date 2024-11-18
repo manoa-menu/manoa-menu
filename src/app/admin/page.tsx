c96a2224e551d6865a8236719c9d2aa9e7609966
Justin <justin@practicalsoftware.com>
2024-11-18 10:58:48 -1000

    Delete more template files

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,5 @@
 import { getServerSession } from 'next-auth';
 import { Container } from 'react-bootstrap';
-// import StuffItemAdmin from '@/components/StuffItemAdmin';
 // import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
@@ -12,7 +11,6 @@ const AdminPage = async () => {
       user: { email: string; id: string; randomKey: string };
     } | null,
   );
-  // const stuff = await prisma.stuff.findMany({});
   // const users = await prisma.user.findMany({});
 
   return (
