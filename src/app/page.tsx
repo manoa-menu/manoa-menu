772fefaea13b670227eaff2277262d58ec76bea0
Adam <147444558+usradam@users.noreply.github.com>
2024-11-25 12:00:34 -1000

    layout change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Col, Container, Row, Image } from 'react-bootstrap';
+import { Col, Container, Row, Card } from 'react-bootstrap';
 import './landingpage.css';
 
 /** The Home page. */
@@ -17,142 +17,35 @@ const Home = () => (
           </h5>
         </Col>
       </Row>
-    </Container>
 
-    {/* Language Section */}
-    <Container fluid className="language-container">
+      {/* Language Section */}
+      <h2 id="features-title"><span>Features</span></h2>
       <Row>
         <Col>
-          <h2 className="language-title"><span>Choose a language</span></h2>
-          <p className="language-subheader"><span>Translate menu items from a UH eatery</span></p>
+          <Card className="language-card">
+            <h5>Choose a language</h5>
+            <p>Choose from English, Japanese, Korean, and Spanish</p>
+          </Card>
         </Col>
-      </Row>
-      <Container className="logo-slider-language">
-        <Container>
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b222e79ee994e012ca7_England.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b259f872fdcb8a7c3af_Spain.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b236291951495107905_France.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b23776b00b1f125bf55_Germany.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0c4b149588661b4440e4_Flags.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b2219f9c81f54469eeb_Chinese.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b24f69d4c07c7d88ada_Italian.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b2380edc120c4301935_Portuguese.png"
-          />
-        </Container>
-
-        <Container>
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b222e79ee994e012ca7_England.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b259f872fdcb8a7c3af_Spain.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b236291951495107905_France.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b23776b00b1f125bf55_Germany.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0c4b149588661b4440e4_Flags.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b2219f9c81f54469eeb_Chinese.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b24f69d4c07c7d88ada_Italian.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b2380edc120c4301935_Portuguese.png"
-          />
-        </Container>
-
-        <Container>
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b222e79ee994e012ca7_England.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b259f872fdcb8a7c3af_Spain.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b236291951495107905_France.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b23776b00b1f125bf55_Germany.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0c4b149588661b4440e4_Flags.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b2219f9c81f54469eeb_Chinese.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b24f69d4c07c7d88ada_Italian.png"
-          />
-          <Image
-            src="https://cdn.prod.website-files.com/6034c69970fc1de43a4bcbc0/656a0b2380edc120c4301935_Portuguese.png"
-          />
-        </Container>
-      </Container>
-    </Container>
-
-    {/* Currency Section */}
-    <Container fluid className="currency-container">
-      <Row>
         <Col>
-          <h2 className="currency-title"><span>Choose a currency</span></h2>
-          <p className="currency-subheader"><span>Have a currency display alongside the dollar value</span></p>
+          <Card className="language-card">
+            <h5>Favorite an item</h5>
+            <p>Save your favorite items for later</p>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="language-card">
+            <h5>Recommended feed</h5>
+            <p>See a curated list of recommended foods</p>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="language-card">
+            <h5>See what&apos;s popular</h5>
+            <p>See what food is currently popular</p>
+          </Card>
         </Col>
       </Row>
-      <Container className="logo-slider-currency">
-        <Container>
-          <h5 className="currency"><span>USD $</span></h5>
-          <h5 className="currency"><span>EUR €</span></h5>
-          <h5 className="currency"><span>JPY ¥</span></h5>
-          <h5 className="currency"><span>GBP £</span></h5>
-          <h5 className="currency"><span>CNY ¥</span></h5>
-          <h5 className="currency"><span>AUD $</span></h5>
-          <h5 className="currency"><span>CAD $</span></h5>
-          <h5 className="currency"><span>CHF Fr</span></h5>
-        </Container>
-
-        <Container>
-          <h5 className="currency"><span>USD $</span></h5>
-          <h5 className="currency"><span>EUR €</span></h5>
-          <h5 className="currency"><span>JPY ¥</span></h5>
-          <h5 className="currency"><span>GBP £</span></h5>
-          <h5 className="currency"><span>CNY ¥</span></h5>
-          <h5 className="currency"><span>AUD $</span></h5>
-          <h5 className="currency"><span>CAD $</span></h5>
-          <h5 className="currency"><span>CHF Fr</span></h5>
-        </Container>
-
-        <Container>
-          <h5 className="currency"><span>USD $</span></h5>
-          <h5 className="currency"><span>EUR €</span></h5>
-          <h5 className="currency"><span>JPY ¥</span></h5>
-          <h5 className="currency"><span>GBP £</span></h5>
-          <h5 className="currency"><span>CNY ¥</span></h5>
-          <h5 className="currency"><span>AUD $</span></h5>
-          <h5 className="currency"><span>CAD $</span></h5>
-          <h5 className="currency"><span>CHF Fr</span></h5>
-        </Container>
-      </Container>
     </Container>
   </main>
 );
