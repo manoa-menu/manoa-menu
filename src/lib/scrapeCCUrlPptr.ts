2eedc98e497b874981785add17238c9fa626cb27
Justin <justin@practicalsoftware.com>
2024-11-27 12:44:26 -1000

    Fix lint error

--- src/lib/scrapeCCUrlPptr.ts
+++ src/lib/scrapeCCUrlPptr.ts
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer-core';
 import { executablePath } from 'puppeteer';
+
 export default async function scrapeCCUrl(url: string): Promise<string> {
   const browser = await puppeteer.launch({
     executablePath: executablePath(), // Use the path to your custom Chromium binary
