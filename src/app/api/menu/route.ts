c3ac2bb660b3a8132f59948494b63e833c984a54
Justin <justin@practicalsoftware.com>
2024-12-01 00:34:30 -1000

    Add JP Holidays & Route Errors

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -10,6 +10,13 @@ export async function GET(req: NextRequest) {
     const menu = await getCheckCCMenu(language);
     return NextResponse.json(menu);
   } catch (error) {
+    if (error instanceof TypeError) {
+      console.error('TypeError fetching menu:', error);
+      return NextResponse.json({ error: 'Type error occurred while fetching the menu.' }, { status: 500 });
+    } if (error instanceof SyntaxError) {
+      console.error('SyntaxError fetching menu:', error);
+      return NextResponse.json({ error: 'Syntax error occurred while fetching the menu.' }, { status: 500 });
+    }
     console.error('Error fetching menu:', error);
     return NextResponse.json({ error: (error as Error).message }, { status: 500 });
   }
