20a15d6a4c8615f75d1256a67247bf395eacc837
Dat Truong <datg@hawaii.edu>
2024-12-01 22:47:44 -1000

    final eslint check

--- src/app/api/getFoodTable/route.ts
+++ src/app/api/getFoodTable/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getFoodTable } from '@/lib/foodTable';
 
+// eslint-disable-next-line import/prefer-default-export
 export async function GET() {
   try {
     const foodTable = await getFoodTable();
