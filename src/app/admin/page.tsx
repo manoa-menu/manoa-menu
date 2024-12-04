1f9ee0772b2e52827456925582d4cdbb8e89f152
Adam <147444558+usradam@users.noreply.github.com>
2024-12-04 12:01:27 -1000

    Admin details and styling

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from 'next-auth';
-import { Container } from 'react-bootstrap';
-// import { prisma } from '@/lib/prisma';
+import { Container, Row, Col } from 'react-bootstrap';
+import { prisma } from '@/lib/prisma';
+import { User } from '@prisma/client';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 
@@ -11,12 +12,20 @@ const AdminPage = async () => {
       user: { email: string; id: string; randomKey: string };
     } | null,
   );
-  // const users = await prisma.user.findMany({});
-
+  const users: User[] = await prisma.user.findMany({});
   return (
     <main>
       <Container id="list" fluid className="py-3">
-        <h1>Cool Admin Page Here! :)</h1>
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {users.map((user) => (
+            <Col key={user.id}>
+              <Row>
+                <h1>{user.email}</h1>
+                <h1>{user.id}</h1>
+              </Row>
+            </Col>
+          ))}
+        </Row>
       </Container>
     </main>
   );
