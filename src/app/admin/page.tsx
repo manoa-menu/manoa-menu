13b636f5de4590706a7ccab059d66fdae5092596
Adam <147444558+usradam@users.noreply.github.com>
2024-12-07 20:07:49 -1000

    Imported missing CSS file

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,6 +5,8 @@ import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 import AdminFoodList from '@/components/AdminFoodList';
 
+import './admin.css';
+
 const AdminPage = async () => {
   const session = await getServerSession(authOptions);
   adminProtectedPage(session as { user: { email: string; id: string; randomKey: string } } | null);
